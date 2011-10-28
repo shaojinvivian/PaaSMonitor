5b0b2df8e861a10ab6a2fb235298a79db477b397
unknown <shaojinvivian@qq.com>
2011-10-28 18:09:04 +0800

    refractor of mxgraph

--- src/main/webapp/app/view/model/Diagram.js
+++ src/main/webapp/app/view/model/Diagram.js
@@ -1,16 +1,51 @@
 Ext.define('PaaSMonitor.view.model.Diagram', {
 	extend : 'Ext.panel.Panel',
 	alias : 'widget.modelDiagram',
-
 	title : 'Model Diagram',
-	contentEl : 'page',
+	layout : 'border',
+	// contentEl : 'diagram',
 	// store : 'AppInstances',
 
 	initComponent : function() {
-		// this.html = '<div id="graphContainer"></div>';	
-		// var start = Ext.get('start-div');
-		// this.add(start);
 		
+		this.items = [
+		
+			{
+				region : 'north',
+				contentEl : 'toolbarContainer',
+				autoHeight : true,
+				border : false,
+				margins : '0 0 5 0'				
+				 }, {
+				 region : 'south',
+				 title : 'South Panel',				
+				 contentEl : 'statusContainer',
+				 split : true,
+				 height : 100,
+				 minHeight : 100
+				  }, {
+				 region : 'west',
+				 title : 'Table',				
+				 contentEl : 'sidebarContainer',				
+				 height : 100,
+				 minHeight : 100
+				  }, {
+				 region : 'center',
+				 title : 'Diagram',				 
+				 contentEl : 'graphContainer',
+				 split : true,
+				 height : 100,
+				 minHeight : 100
+				   }, {
+				 region : 'east',
+				 title : 'Outline',				 
+				 contentEl : 'outlineContainer',
+				 split : true,
+				 height : 100,
+				 minHeight : 100
+				 
+			}			];
+			
 		this.callParent(arguments);
 	}
 });
